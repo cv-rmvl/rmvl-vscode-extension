b6fc1621967337e8ece9a779ba13498b7d4a6bea
zhaoxi <535394140@qq.com>
2023-10-18 22:15:24 +0800

    新增 CMake 功能，完善 rmvl.para 扩展 - rmvl.para 标识符允许匹配单个字符 - CMake 扩展添加快捷包含 RMVL 的功能

--- src/extension.js
+++ src/extension.js
@@ -2,62 +2,68 @@ const vscode = require('vscode');
 
 function activate(context) {
     ///////////////////////// CMake for RMVL /////////////////////////
-    const cmakeExtensionProvider = vscode.languages.registerCompletionItemProvider('cmake', {
+    const cmakeFunctionsProvider = vscode.languages.registerCompletionItemProvider('cmake', {
         provideCompletionItems() {
             // compile definition
             const rmvlCompileDefinition = new vscode.CompletionItem('rmvl_compile_definitions', vscode.CompletionItemKind.Function);
-            rmvlCompileDefinition.insertText = new vscode.SnippetString("rmvl_compile_definitions(\n  ${1:module_name}\n  ${2|PUBLIC,INTERFACE,PRIAVTE|} ${3:MY_DR}\n)");
+            rmvlCompileDefinition.insertText = new vscode.SnippetString('rmvl_compile_definitions(\n  ${1:module_name}\n  ${2|PUBLIC,INTERFACE,PRIAVTE|} ${3:MY_DR}\n)');
             rmvlCompileDefinition.documentation = new vscode.MarkdownString(
-                "#### 将预处理定义添加至指定目标\n##### 用法:\n```\nrmvl_compile_definitions(<target>\n  <INTERFACE | PUBLIC | PRIVATE> [items1...]\n  [<INTERFACE | PUBLIC | PRIVATE> [items2...] ...])\n```\n##### 示例:\n```\nrmvl_compile_definitions(\n  aaa\n  INTERFACE HAVE_AAA\n)\n```"
+                `#### 将预处理定义添加至指定目标\n##### 用法:\n\`\`\`\nrmvl_compile_definitions(<target>\n  <INTERFACE | PUBLIC | PRIVATE> [items1...]\n  [<INTERFACE | PUBLIC | PRIVATE> [items2...] ...])\n\`\`\`\n##### 示例:\n\`\`\`\nrmvl_compile_definitions(\n  aaa\n  INTERFACE HAVE_AAA\n)\n\`\`\``
             );
             // install directories
             const rmvlInstallDirectories = new vscode.CompletionItem('rmvl_install_directories', vscode.CompletionItemKind.Function);
-            rmvlInstallDirectories.insertText = new vscode.SnippetString("rmvl_install_directories(\n  ${1:where_to_install}\n)");
+            rmvlInstallDirectories.insertText = new vscode.SnippetString('rmvl_install_directories(\n  ${1:where_to_install}\n)');
             rmvlInstallDirectories.documentation = new vscode.MarkdownString(
-                "#### 将指定路径下的所有文件安装至特定目标\n##### 用法:\n```\nrmvl_install_directories(<directory> [DST_LIB])\n```\n##### 示例:\n```\nrmvl_install_directories(include/rmvl)\n```"
+                '#### 将指定路径下的所有文件安装至特定目标\n##### 用法:\n```\nrmvl_install_directories(<directory> [DST_LIB])\n```\n##### 示例:\n```\nrmvl_install_directories(include/rmvl)\n```'
             );
             // add module
             const rmvlAddModule = new vscode.CompletionItem('rmvl_add_module', vscode.CompletionItemKind.Function);
-            rmvlAddModule.insertText = new vscode.SnippetString("rmvl_add_module(\n  ${1:module_name}\n  DEPENDS ${2:core}\n)");
+            rmvlAddModule.insertText = new vscode.SnippetString('rmvl_add_module(\n  ${1:module_name}\n  DEPENDS ${2:core}\n)');
             rmvlAddModule.documentation = new vscode.MarkdownString(
-                "#### 在当前目录中添加新的 RMVL 模块\n##### 用法:\n```\nrmvl_add_module(<module_name> [INTERFACE] [EXTRA_HEADER <list of other include directories>]\n  [EXTRA_SOURCE <list of other source directories>] [DEPENDS <list of rmvl dependencies>]\n  [EXTERNAL <list of 3rd party dependencies>])\n```\n##### 示例:\n```\nrmvl_add_module(\n  my_module               # 需要生成的模块 (文件夹名)\n  EXTRA_HEADER xxx_h      # 参与构建的其余头文件目录\n  EXTRA_SOURCE xxx_src    # 参与构建的其余头文件目录\n  DEPENDS core            # 依赖的 RMVL 模块 (文件夹名)\n  EXTERNAL ${OpenCV_LIBS} # 依赖的第三方目标库\n)\n```"
+                `#### 在当前目录中添加新的 RMVL 模块\n##### 用法:\n\`\`\`\nrmvl_add_module(<module_name> [INTERFACE] [EXTRA_HEADER <list of other include directories>]\n  [EXTRA_SOURCE <list of other source directories>] [DEPENDS <list of rmvl dependencies>]\n  [EXTERNAL <list of 3rd party dependencies>])\n\`\`\`\n##### 示例:\n\`\`\`\nrmvl_add_module(\n  my_module               # 需要生成的模块 (文件夹名)\n  EXTRA_HEADER xxx_h      # 参与构建的其余头文件目录\n  EXTRA_SOURCE xxx_src    # 参与构建的其余头文件目录\n  DEPENDS core            # 依赖的 RMVL 模块 (文件夹名)\n  EXTERNAL \${OpenCV_LIBS} # 依赖的第三方目标库\n)\n\`\`\``
             );
             // compile options
             const rmvlCompileOptions = new vscode.CompletionItem('rmvl_compile_options', vscode.CompletionItemKind.Function);
-            rmvlCompileOptions.insertText = new vscode.SnippetString("rmvl_compile_options(\n  ${1:module_name}\n  ${2|PUBLIC,INTERFACE,PRIVATE|} options\n)");
+            rmvlCompileOptions.insertText = new vscode.SnippetString('rmvl_compile_options(\n  ${1:module_name}\n  ${2|PUBLIC,INTERFACE,PRIVATE|} options\n)');
             rmvlCompileOptions.documentation = new vscode.MarkdownString(
-                "#### 将编译选项添加至指定目标\n##### 用法:\n```\nrmvl_compile_options(<target> [BEFORE]\n  <INTERFACE|PUBLIC|PRIVATE> [items1...]\n  [<INTERFACE|PUBLIC|PRIVATE> [items2...] ...])\n```\n##### 示例:\n```\nrmvl_compile_options(\n  my_module  # RMVL 目标名\n  PRIVATE -w # 编译选项\n)\n```"
+                `#### 将编译选项添加至指定目标\n##### 用法:\n\`\`\`\nrmvl_compile_options(<target> [BEFORE]\n  <INTERFACE|PUBLIC|PRIVATE> [items1...]\n  [<INTERFACE|PUBLIC|PRIVATE> [items2...] ...])\n\`\`\`\n##### 示例:\n\`\`\`\nrmvl_compile_options(\n  my_module  # RMVL 目标名\n  PRIVATE -w # 编译选项\n)\n\`\`\``
             );
             // add test
             const rmvlAddTest = new vscode.CompletionItem('rmvl_add_test', vscode.CompletionItemKind.Function);
-            rmvlAddTest.insertText = new vscode.SnippetString("rmvl_add_test(\n  ${2:test_name} ${1|Unit,Performance|}\n  DEPENDS ${2:module_name}\n  DEPEND_TESTS ${2:GTest::gtest_main}\n)");
+            rmvlAddTest.insertText = new vscode.SnippetString('rmvl_add_test(\n  ${2:test_name} ${1|Unit,Performance|}\n  DEPENDS ${2:module_name}\n  DEPEND_TESTS ${2:GTest::gtest_main}\n)');
             rmvlAddTest.documentation = new vscode.MarkdownString(
-                "#### 此命令用于为指定模块添加新的 RMVL 测试\n##### 用法:\n```\nrmvl_add_test(<name> <Unit|Performance> <DEPENDS> [rmvl_target...]\n  <DEPEND_TESTS> [test_target...])\n```\n##### 示例:\n```\nrmvl_add_test(\n  detector Unit                  # 测试名\n  DEPENDS armor_detector         # 需要依赖的 RMVL 目标库\n  DEPEND_TESTS GTest::gtest_main # 需要依赖的第三方测试工具目标库\n)\n```"
+                `#### 此命令用于为指定模块添加新的 RMVL 测试\n##### 用法:\n\`\`\`\nrmvl_add_test(<name> <Unit|Performance> <DEPENDS> [rmvl_target...]\n  <DEPEND_TESTS> [test_target...])\n\`\`\`\n##### 示例:\n\`\`\`\nrmvl_add_test(\n  detector Unit                  # 测试名\n  DEPENDS armor_detector         # 需要依赖的 RMVL 目标库\n  DEPEND_TESTS GTest::gtest_main # 需要依赖的第三方测试工具目标库\n)\n\`\`\``
             );
             // add exeutable
             const rmvlAddExe = new vscode.CompletionItem('rmvl_add_exe', vscode.CompletionItemKind.Function);
-            rmvlAddExe.insertText = new vscode.SnippetString("rmvl_add_exe(\n  ${1:sample_name}\n  SOURCES ${1:file_name}.cpp\n  DEPENDS ${2:module_name}\n)");
+            rmvlAddExe.insertText = new vscode.SnippetString('rmvl_add_exe(\n  ${1:sample_name}\n  SOURCES ${1:file_name}.cpp\n  DEPENDS ${2:module_name}\n)');
             rmvlAddExe.documentation = new vscode.MarkdownString(
-                "#### 此命令用于为指定模块添加新的 RMVL 可执行文件\n##### 用法:\n```\nrmvl_add_exe(<name> SOURCES <file_name>\n  [DEPENDS <list of rmvl dependencies>]\n  [EXTERNAL <list of 3rd party dependencies>]\n)\n```\n##### 示例:\n```\nrmvl_add_exe(\n  demo             # 可执行文件名（包含 rmvl_ 前缀）\n  SOURCES demo.cpp # 源文件\n  DEPENDS core     # 依赖的 RMVL 模块\n)\n```"
+                '#### 此命令用于为指定模块添加新的 RMVL 可执行文件\n##### 用法:\n```\nrmvl_add_exe(<name> SOURCES <file_name>\n  [DEPENDS <list of rmvl dependencies>]\n  [EXTERNAL <list of 3rd party dependencies>]\n' +
+                ')\n```\n##### 示例:\n```\nrmvl_add_exe(\n  demo             # 可执行文件名（包含 rmvl_ 前缀）\n  SOURCES demo.cpp # 源文件\n  DEPENDS core     # 依赖的 RMVL 模块\n)\n```'
             );
             // set properties
             const rmvlSetProperties = new vscode.CompletionItem('rmvl_set_properties', vscode.CompletionItemKind.Function);
-            rmvlSetProperties.insertText = new vscode.SnippetString("rmvl_set_properties(\n  ${1:module_name} PROPERTIES\n  ${2:properties}\n)");
+            rmvlSetProperties.insertText = new vscode.SnippetString('rmvl_set_properties(\n  ${1:module_name} PROPERTIES\n  ${2:properties}\n)');
             rmvlSetProperties.documentation = new vscode.MarkdownString(
-                "设置如何构建指定 Target 的属性\n用法:\n```\nrmvl_set_properties(target1 target2 ...\n  PROPERTIES prop1 value1\n  prop2 value2 ...)\n```\n示例:\n```\nrmvl_set_properties(\n  detector                   # 目标名\n  PROPERTIES CXX_STANDARD 17 # 属性\n)\n```"
+                `设置如何构建指定 Target 的属性\n用法:\n\`\`\`\nrmvl_set_properties(target1 target2 ...\n  PROPERTIES prop1 value1\n  prop2 value2 ...)\n\`\`\`\n示例:\n\`\`\`\nrmvl_set_properties(\n  detector                   # 目标名\n  PROPERTIES CXX_STANDARD 17 # 属性\n)\n\`\`\``
             );
             // download
             const rmvlDownload = new vscode.CompletionItem('rmvl_download', vscode.CompletionItemKind.Function);
-            rmvlDownload.insertText = new vscode.SnippetString("rmvl_download(\n  \${2:module_name} \${1|GIT,URL|}\n  \${0:address_git_or_url}\n)");
+            rmvlDownload.insertText = new vscode.SnippetString('rmvl_download(\n  ${2:module_name} ${1|GIT,URL|}\n  ${0:address_git_or_url}\n)');
             rmvlDownload.documentation = new vscode.MarkdownString(
-                "#### 下载并参与 RMVL 的构建\n##### 用法:\n```\nrmvl_download(\n  <dl_name> <dl_kind>\n  <...>\n)\n```\n##### 示例 1:\n```\nrmvl_download(\n  benchmark GIT\n  xxx : master # 仓库地址与分支/标签名\n)\n```\n##### 示例 2\n```\nrmvl_download(\n  googletest URL\n  xxx # 要下载的路径（一般是压缩文件）\n)\n```"
+                `#### 下载并参与 RMVL 的构建\n##### 用法:\n\`\`\`\nrmvl_download(\n  <dl_name> <dl_kind>\n  <...>\n)\n\`\`\`\n##### 示例 1:\n\`\`\`\nrmvl_download(\n  benchmark GIT\nxxx : master # 仓库地址与分支/标签名\n)\n\`\`\`\n##### 示例 2\n\`\`\`\nrmvl_download(\n  googletest URL\n  xxx # 要下载的路径（一般是压缩文件）\n)\n\`\`\``
             );
+            // 初始化并包含 RMVL
+            const includeRMVL = new vscode.CompletionItem('RMVL', vscode.CompletionItemKind.Snippet);
+            includeRMVL.insertText = new vscode.SnippetString(
+                `# include rmvl\nfind_package(RMVL REQUIRED)\nlist(APPEND CMAKE_MODULE_PATH \\\${RMVL_DIR})\n# use rmvl functions and macros\ninclude(RMVLModule)\ninclude(RMVLUtils)\n\n`
+            );
+            includeRMVL.documentation = new vscode.MarkdownString(`使用 \`find_package\` 寻找 RMVL，并包含相关模块，包含\n- \`RMVLModule\``);
 
             return [
-                rmvlCompileDefinition, rmvlInstallDirectories,
-                rmvlAddModule, rmvlCompileOptions,
-                rmvlAddTest, rmvlAddExe,
-                rmvlSetProperties, rmvlDownload
+                rmvlCompileDefinition, rmvlInstallDirectories, rmvlAddModule, rmvlCompileOptions,
+                rmvlAddTest, rmvlAddExe, rmvlSetProperties, rmvlDownload,
+                includeRMVL
             ];
         }
     });
@@ -87,23 +93,29 @@ function activate(context) {
                 completionItems.push(new vscode.CompletionItem(type, vscode.CompletionItemKind.Keyword));
             }
             for (const classList of classLists) {
-                completionItems.push(new vscode.CompletionItem(classList, vscode.CompletionItemKind.Class));
+                const item = new vscode.CompletionItem(classList, vscode.CompletionItemKind.Class);
+                if (classList === 'vector') {
+                    item.insertText = new vscode.SnippetString('vector<${0}>');
+                }
+                completionItems.push(item);
             }
 
             // snippet
             const paraSnippet = new vscode.CompletionItem('ParaHelp', vscode.CompletionItemKind.Snippet);
             paraSnippet.insertText = new vscode.SnippetString(
-                "###### 下列代码均为示例代码，使用前需删除 ######\n" +
-                "int NUMERIC_DEMO = 12       # 整型数字示例\n" +
-                "float FLOAT_DEMO = 3.1f     # 浮点型数字示例\n" +
-                "string STR_DEMO = \"ab12+/?\" # 字符串示例\n" +
-                "vector<Point> POINTS_DEMO = {{1, 1}, \\\n" +
-                "                             {-2, 30}, \\\n" +
-                "                             {3, 7}} # vector 表示的点集示例\n" +
-                "Vec3d VEC3F_DEMO = {3.1, 2.2, 1.3}   # 向量示例\n" +
-                "################################################\n"
+                '######### 下列代码均为示例代码，使用前需删除 #########\n' +
+                'int NUMERIC_DEMO = 12               # 整型数字示例\n' +
+                'float FLOAT_DEMO = 3.1f             # 浮点型数字示例\n' +
+                'string STR_DEMO = \'ab12+/?\'         # 字符串示例\n' +
+                'vector<int> VECTOR_DEMO = {1, 2, 3} # 整型列表示例 \n' +
+                'Matx33f MATX33_DEMO = {1, 2, 3, \\\n' +
+                '                       4, 5, 6, \\\n' +
+                '                       7, 8, 9}    # 3x3 矩阵示例\n' +
+                'Vec3d VEC3F_DEMO = {3.1, 2.2, 1.3} # 向量示例\n' +
+                'Vec4d VEC4_DEMO = Vec4d::ones()    # 使用函数的向量示例\n' +
+                '######################################################\n'
             );
-            paraSnippet.documentation = new vscode.MarkdownString("显示 RMVL Parameters 的示例代码");
+            paraSnippet.documentation = new vscode.MarkdownString('显示 RMVL Parameters 的示例代码');
 
             completionItems.push(paraSnippet);
 
@@ -134,8 +146,8 @@ function activate(context) {
         }
     }, ':'); // triggered whenever a ':' is being typed
 
-    /////////////////////////// Characters ///////////////////////////
-    const charactersExtensionProvider = vscode.languages.registerCompletionItemProvider('rmvl.para', {
+    ///////////////////////// Characters '(' /////////////////////////
+    const charactersProvider = vscode.languages.registerCompletionItemProvider('rmvl.para', {
         provideCompletionItems(document, position) {
             if (document.lineAt(position).text.substring(0, position.character).endsWith('(')) {
                 vscode.window.activeTextEditor.insertSnippet(
@@ -146,14 +158,13 @@ function activate(context) {
         }
     }, '(');
 
-
     context.subscriptions.push(
-        // CMake 支持
-        cmakeExtensionProvider,
-        // RMVL Parameter
+        // RMVL CMake 扩展
+        cmakeFunctionsProvider,
+        // RMVL Parameter 扩展
         rmvlparaKeywordsProvider, rmvlparaFunctionsProvider,
         // 符号扩展
-        charactersExtensionProvider
+        charactersProvider
     );
 }
 
